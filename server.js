e50c269a543ca1803ce06faf7c7e35b992e03724
Sifiso Shezi <Sifiso@Sifisos-MacBook-Pro.local>
2016-02-11 13:02:24 +0200

    add profile route

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var express = require('express');
 var stormpath = require('express-stormpath');
 
 var app = express();
-
+var profile = require('./profile')();
+console.log(profile);
 app.set('views','./views');
 app.set('view engine','jade');
 
@@ -11,6 +12,10 @@ app.use(stormpath.init(app,{ website : true , expand:{customdata : true }}));
 app.get('/',function(req,res){
 	res.render('home',{ title :'Welcome'});
 });
+app.get('/test',function(req,res){
+    res.send('send');
+});
+app.get('/profile',stormpath.loginRequired,profile);
 
 app.on('stormpath.ready',function(){
 	console.log('Storm path ready');
